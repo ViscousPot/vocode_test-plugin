ac03b8ab2991bb7651aecdcd1190420d1b9f8a9a
ViscousPot <viscouspotential@gmail.com>
2025-07-03 21:24:49 +0100

    Update plugin.lua

--- plugin.lua
+++ plugin.lua
@@ -67,8 +67,6 @@ function add(settings, data)
     position = file.getPosition()
     local line = file.readForwardLine()
 
-    print(line)
-
     if (string.find(line, searchString)) then 
 
       if editOffset < 0 then
